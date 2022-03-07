d550f97aaf38b57ab033d75e09f86fa2ac349866
Bruno <bruno.buck1@hotmail.com>
2022-03-07 00:01:28 -0300

    Create DeleteProducts

---
+++ src/pages/DeleteProducts/index.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from "react";
+import "./styles.scss";
+
+import Layout from "../../components/Layout";
+import { api } from "../../services/api";
+import { soNumero } from "../../Utils";
+import { useParams, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+function EditProduct() {
+  const [modal, setModal] = useState([]);
+
+  let navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/produto-details/${id}`)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setModal(data);
+      });
+  }, [id]);
+
+  const { register, handleSubmit } = useForm();
+
+  async function putProcuct() {
+    await api.delete(`/produto/${id}`);
+    navigate("/products");
+  }
+
+  return (
+    <Layout>
+      <div className="wrapper">
+        <form onSubmit={handleSubmit(putProcuct)} className="form-put">
+          <input
+            type="text"
+            {...register("name_product")}
+            placeholder="title"
+            defaultValue={modal?.name_product}
+            autoFocus
+          />
+          <textarea
+            type="text"
+            {...register("description_product")}
+            placeholder="title"
+            defaultValue={modal?.description_product}
+          />
+          <input
+            type="text"
+            className="date"
+            {...register("price_product")}
+            defaultValue={modal?.price_product}
+            onKeyUp={soNumero}
+          />
+          <input
+            type="text"
+            {...register("qt_product")}
+            placeholder="title"
+            defaultValue={modal?.qt_product}
+            onKeyUp={soNumero}
+          />
+          <div className="buttons">
+            <button type="submit">Confirm</button>
+            <button
+              type="button"
+              onClick={() => {
+                navigate("/products");
+              }}
+            >
+              &#8592; back
+            </button>
+          </div>
+        </form>
+      </div>
+    </Layout>
+  );
+}
+
+export default EditProduct;
--- src/routes.jsx
+++ src/routes.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 
 import AddProducts from './pages/AddProducts'
 import Customers from './pages/Customers'
+import DeleteProduct from './pages/DeleteProducts';
 import EditProduct from './pages/EditProduct';
 import Home from './pages/home'
 import NotFound from './pages/404'
@@ -14,6 +15,7 @@ function Routers() {
             <Route path='/' element={<Home/>} />
             <Route path='/add-products' element={<AddProducts />} />
             <Route path='/customers' element={<Customers/>} />
+            <Route path='/delete-product/:id' element={<DeleteProduct />} />
             <Route path='/edit-product/:id' element={<EditProduct />} />
             <Route path='/products' element={<Products />} />
             <Route path="*" element={<NotFound/>} />
